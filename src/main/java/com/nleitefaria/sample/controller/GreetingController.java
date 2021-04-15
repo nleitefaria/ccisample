875ba23f48ffa7ef7fd580b24c6f001313cbf075
User01 <nleitefaria@outlook.com>
2021-04-15 14:15:00 +0100

    Updated GreetingController

--- src/main/java/com/nleitefaria/sample/controller/GreetingController.java
+++ src/main/java/com/nleitefaria/sample/controller/GreetingController.java
@@ -1,16 +1,17 @@
 package com.nleitefaria.sample.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nleitefaria.sample.model.Greeting;
 
 @RestController
-public class GreetingController {
-    
+public class GreetingController 
+{
     @GetMapping("/greeting")
-    public Greeting greeting() {
-        return new Greeting("Salut");
+    public ResponseEntity<Greeting> greeting() {
+        return ResponseEntity.ok(new Greeting("Salut"));
     }
 
 }
